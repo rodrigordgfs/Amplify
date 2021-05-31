a73bcaa6136d9c93e90c195e05fb2eefb91d293f
Rodrigo Viegas Rodrigues <rodrigoviegas@lojaspompeia.com.br>
2021-05-31 15:16:00 -0300

    Hamburguer Menu

--- js/script.js
+++ js/script.js
@@ -10,3 +10,29 @@ if (window.SimpleSlide) {
 if (window.SimpleAnime) {
     new SimpleAnime()
 }
+
+const btnMobile = document.getElementById('btn-mobile');
+const menu = document.getElementById('menu');
+const nav = document.getElementById('nav');
+
+function toogleMenu(event) {
+    if (event.type === 'touchStart') event.preventDefault();
+    nav.classList.toggle('active');
+    const active = nav.classList.contains('active');
+    event.currentTarget.setAttribute('aria-expanded', active)
+    if (active) {
+        event.currentTarget.setAttribute('aria-label', 'Fechar menu')
+    } else {
+        event.currentTarget.setAttribute('aria-label', 'Abrir menu')
+    }
+}
+
+function toogleItemMenu(event) {
+    if (event.type === 'touchStart') event.preventDefault();
+    nav.classList.toggle('active');
+}
+
+btnMobile.addEventListener('click', toogleMenu);
+btnMobile.addEventListener('touchStart', toogleMenu);
+menu.addEventListener('click', toogleItemMenu);
+menu.addEventListener('touchStart', toogleItemMenu);
